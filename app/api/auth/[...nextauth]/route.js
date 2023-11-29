9751567e17e455a38c68a8f36d34012bd662c2c9
CarrioliDante <carriolidante@gmail.com>
2023-11-29 19:25:58 -0300

    [ADD] Post-feature

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,43 +1,45 @@
-import NextAuth from 'next-auth/next'
+import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { connectToDB } from '@utils/database'
+
 import User from '@models/user'
+import { connectToDB } from '@utils/database'
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientID: process.env.GOOGLE_ID,
+      clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    })
-    session.user.id = sessionUser._id.toString()
-    return session
-  },
-  async signIn({ profile }) {
-    try {
-      //Serverless -> lambda ->Cada vez que es llamada
-      await connectToDB()
-      //Debemos crear un modal
-      //Chequear que el usuario exista
-      const userExist = await User.findOne({ email: profile.email })
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email })
+      session.user.id = sessionUser._id.toString()
 
-      //Chequear si no existe crear usuario y guardarlo en DB
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        })
+      return session
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB()
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email })
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log('Error checking if user exists: ', error.message)
+        return false
       }
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
-    }
+    },
   },
 })
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,44 +1,51 @@
 'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+
 const Nav = () => {
-  const isUserLoggedIn = true
-  const [provider, setProviders] = useState(null)
+  const { data: session } = useSession()
+
+  const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
+
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders()
-      setProviders(response)
-    }
-    setProviders
+    ;(async () => {
+      const res = await getProviders()
+      setProviders(res)
+    })()
   }, [])
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.svg'
-          alt='Prompter logo'
+          alt='logo'
           width={30}
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Prompter</p>
+        <p className='logo_text'>Promptopia</p>
       </Link>
-      {/* Desk Nav */}
+
+      {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link className='black_btn' href='/create-prompt'>
-              Create post
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
             </Link>
-            <button type='button' onClick={signOut} className='outline-btn'>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
               Sign Out
             </button>
+
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -53,25 +60,28 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id)
+                  }}
                   className='black_btn'>
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
         )}
       </div>
-      {/* Mobile nav */}
+
+      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
@@ -91,7 +101,7 @@ const Nav = () => {
                 <button
                   type='button'
                   onClick={() => {
-                    toggleDropdown(false)
+                    setToggleDropdown(false)
                     signOut()
                   }}
                   className='mt-5 w-full black_btn'>
@@ -107,9 +117,11 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id)
+                  }}
                   className='black_btn'>
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
