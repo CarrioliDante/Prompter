a4132127fcab9398eac95359370016105fc4cb0c
CarrioliDante <carriolidante@gmail.com>
2023-11-29 00:35:45 -0300

    wip

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 export const metadata = {
   title: 'Pompter',
   description: 'Discover & share AI Promts',
@@ -8,10 +10,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang='es'>
       <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>{children}</main>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
+import Feed from '@components/Feed'
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
-        Discover and Share
+        Discover and Share <br />
         <br className='max-md:hidden' />
         <span className='orange_gradient text-center'>AI-Powered Prompts</span>
       </h1>
@@ -10,7 +11,7 @@ const Home = () => {
         Prompter is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
-      {/* feed */}
+      <Feed />
     </section>
   )
 }
